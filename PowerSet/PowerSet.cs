612f41662a41821270ccf788fb02fceb315a7a7d
Гапеев Дмитрий Игоревич <Dmitriy.Gapeev@galaktika.by>
2019-01-24 16:49:45 +0300

    Тесты

---
+++ PowerSet/PowerSet.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace AlgorithmsDataStructures
+{
+
+	// наследуйте этот класс от HashTable
+	// или расширьте его методами из HashTable
+	public class PowerSet<T>
+	{
+		public T[] _values = new T[4];
+		public SlotStatus[] _slotsStatus = new SlotStatus[4];
+		private int _step = 3;
+		private int _count = 0;
+
+		public enum SlotStatus
+		{
+			Empty,
+			Fill
+		}
+
+		private void ReHash()
+		{
+			T[] oldArray = _values;
+			SlotStatus[] oldStatusArray = _slotsStatus;
+
+			_values = new T[_count * 2];
+			_slotsStatus = new SlotStatus[_count * 2];
+			_count = 0;
+
+			for (int i = 0; i < oldArray.Length; i++)
+			{
+				if (oldStatusArray[i] == SlotStatus.Fill)
+					Put(oldArray[i]);
+			}
+		}
+
+		private int HashFunc(T value)
+		{
+			int hash = Math.Abs(value.GetHashCode());
+			return hash % _values.Length;
+		}
+
+		private int SeekSlot(T value)
+		{
+			int position = HashFunc(value);
+
+			for (int i = 0; i < _slotsStatus.Length; i++)
+			{
+				if (_slotsStatus[position] == SlotStatus.Empty)
+					return position;
+
+				position += _step;
+				position %= _slotsStatus.Length;
+			}
+
+			return -1;
+		}
+
+		private int GetSlot(T value)
+		{
+			int position = HashFunc(value);
+			for (int i = 0; i < _slotsStatus.Length; i++)
+			{
+				if (_slotsStatus[position] == SlotStatus.Fill && _values[position].Equals(value))
+					return position;
+
+				position += _step;
+				position %= _slotsStatus.Length;
+			}
+
+			return -1;
+		}
+
+		public PowerSet()
+		{
+			for (int i = 0; i < _slotsStatus.Length; i++)
+				_slotsStatus[i] = SlotStatus.Empty;
+		}
+
+		public int Size()
+		{
+			return _count;
+		}
+
+		public void Put(T value)
+		{
+			if (!Get(value))
+			{
+				int position = SeekSlot(value);
+
+				if (position != -1)
+				{
+					_values[position] = value;
+					_slotsStatus[position] = SlotStatus.Fill;
+					_count++;
+					if (_count == _values.Length)
+						ReHash();
+				}
+				else
+					ReHash();
+			}
+		}
+
+
+		public bool Get(T value)
+		{
+			return GetSlot(value) != -1;
+		}
+
+		public bool Remove(T value)
+		{
+			int position = GetSlot(value);
+
+			if (position != -1)
+			{
+				_values[position] = default(T);
+				_slotsStatus[position] = SlotStatus.Empty;
+				_count--;
+			}
+
+			return position != -1;
+		}
+
+		public PowerSet<T> Intersection(PowerSet<T> set2)
+		{
+
+			if (set2 == null)
+				return null;
+
+			PowerSet<T> resultSet = new PowerSet<T>();
+
+			for (int i = 0; i < set2._values.Length; i++)
+			{
+				T value = set2._values[i];
+				if (set2._slotsStatus[i] == SlotStatus.Fill && Get(value))
+					resultSet.Put(value);
+			}
+
+			return resultSet;
+		}
+
+		public PowerSet<T> Union(PowerSet<T> set2)
+		{
+			if (set2 == null)
+				return null;
+
+			PowerSet<T> resultSet = new PowerSet<T>();
+
+			for (int i = 0; i < _values.Length; i++)
+			{
+				if (_slotsStatus[i] == SlotStatus.Fill)
+					resultSet.Put(_values[i]);
+			}
+
+			for (int i = 0; i < set2._values.Length; i++)
+			{
+				if (set2._slotsStatus[i] == SlotStatus.Fill)
+					resultSet.Put(set2._values[i]);
+			}
+
+			return resultSet;
+		}
+
+		public PowerSet<T> Difference(PowerSet<T> set2)
+		{
+			if (set2 == null)
+				return null;
+
+			PowerSet<T> resultSet = new PowerSet<T>();
+
+			for (int i = 0; i < _values.Length; i++)
+			{
+				T currentValue = _values[i];
+				if (_slotsStatus[i] == SlotStatus.Fill && !set2.Get(currentValue))
+					resultSet.Put(currentValue);
+			}
+
+			return resultSet;
+		}
+
+		public bool IsSubset(PowerSet<T> set2)
+		{
+			if (set2 == null)
+				return false;
+
+			bool result = true;
+
+			for (int i = 0; i < set2._values.Length; i++)
+			{
+				if (set2._slotsStatus[i] == SlotStatus.Fill && !Get(set2._values[i]))
+				{
+					result = false;
+					break;
+				}
+			}
+
+			return result;
+		}
+	}
+}
--- PowerSet/Program.cs
+++ PowerSet/Program.cs
@@ -1,16 +1,168 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PowerSet
+namespace AlgorithmsDataStructures
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
+		  //1. 2 пустых
+			//2. пустой, со значениями
+			//3. со значениями, пустой
+			//4. больше значений, меньше значений
+			//5. меньше значений, больше
+			//6. нет общих
+			
+			//TestPowerSet();
 
+			TestIntersection(CreateEmptyPowerSet(), CreateEmptyPowerSet());
+			TestIntersection(CreateEmptyPowerSet(), CreatePowerSet(10, 20));
+
+			TestIntersection(CreatePowerSet(10,20), CreateEmptyPowerSet());
+			TestIntersection(CreatePowerSet(10, 50), CreatePowerSet(20, 35));
+
+			TestIntersection(CreatePowerSet(20, 25), CreatePowerSet(10, 50));
+			TestIntersection(CreatePowerSet(10, 20), CreatePowerSet(30, 40));
+
+
+
+			Console.ReadKey();
+		}
+
+		static void TestPowerSet()
+		{
+			Console.WriteLine("test power set");
+			PowerSet<int> powerSet = new PowerSet<int>();
+
+			for (int i = 0; i <= 10; i++)
+				powerSet.Put(i);
+
+			for (int i = 0; i < powerSet._values.Length; i++)
+				Console.Write(powerSet._values[i] + " ");
+
+			Console.WriteLine();
+			Console.WriteLine("test put 11");
+			Console.WriteLine("power set size before  = " + powerSet.Size());
+			powerSet.Put(11);
+			Console.WriteLine("power set size after = " + powerSet.Size());
+
+			for (int i = 0; i < powerSet._values.Length; i++)
+				Console.Write(powerSet._values[i] + " ");
+
+			Console.WriteLine();
+
+			Console.WriteLine("test put 10");
+			Console.WriteLine("power set size before  = " + powerSet.Size());
+			powerSet.Put(10);
+			Console.WriteLine("power set size after = " + powerSet.Size());
+
+			for (int i = 0; i < powerSet._values.Length; i++)
+				Console.Write(powerSet._values[i] + " ");
+
+			Console.WriteLine();
+			Console.WriteLine("test remove 4");
+			Console.WriteLine("power set size before  = " + powerSet.Size());
+			bool result = powerSet.Remove(4);
+			Console.WriteLine("power set size after = " + powerSet.Size());
+			Console.WriteLine("remove result = " + result);
+			for (int i = 0; i < powerSet._values.Length; i++)
+				Console.Write(powerSet._values[i] + " ");
+
+			Console.WriteLine();
+			Console.WriteLine("test remove 4");
+			Console.WriteLine("power set size before  = " + powerSet.Size());
+		  result = powerSet.Remove(4);
+			Console.WriteLine("power set size after = " + powerSet.Size());
+			Console.WriteLine("remove result = " + result);
+			for (int i = 0; i < powerSet._values.Length; i++)
+				Console.Write(powerSet._values[i] + " ");
+
+			Console.WriteLine();
+			Console.WriteLine("test remove 20");
+			Console.WriteLine("power set size before  = " + powerSet.Size());
+		  result = powerSet.Remove(20);
+			Console.WriteLine("power set size after = " + powerSet.Size());
+			Console.WriteLine("remove result = " + result);
+			for (int i = 0; i < powerSet._values.Length; i++)
+				Console.Write(powerSet._values[i] + " ");
+
+			Console.WriteLine("test get");
+
+			for (int i = 0; i < powerSet._values.Length; i++)
+			{
+				Console.WriteLine(i + " get result = " + powerSet.Get(i));
+			}
+
+			Console.WriteLine();
+			Console.WriteLine(new string('=', 50));
+
+		}
+
+		static void TestIntersection(PowerSet<int> set1, PowerSet<int> set2)
+		{
+			if(set1.Size() == 0 )
+				Console.Write("set1 is empty");
+			else
+			{
+				Console.WriteLine("set1");
+				for (int i = 0; i < set1._values.Length; i++)
+				{
+					if(set1._slotsStatus[i] == PowerSet<int>.SlotStatus.Fill)
+						Console.Write(set1._values[i] + " ");
+				}
+			}
+
+			Console.WriteLine();
+
+			if(set2.Size() == 0)
+				Console.Write("set2 is empty");
+			else
+			{
+				Console.WriteLine("set2");
+				for (int i = 0; i < set2._values.Length; i++)
+				{
+					if (set2._slotsStatus[i] == PowerSet<int>.SlotStatus.Fill)
+						Console.Write(set2._values[i] + " ");
+				}
+			}
+
+			Console.WriteLine();
+
+			PowerSet<int> resultSet = set1.Intersection(set2);
+			if (resultSet.Size() == 0)
+				Console.Write("result set is empty");
+			else
+			{
+				Console.WriteLine("result set");
+				for (int i = 0; i < resultSet._values.Length; i++)
+				{
+					if (resultSet._slotsStatus[i] == PowerSet<int>.SlotStatus.Fill)
+						Console.Write(resultSet._values[i] + " ");
+				}
+			}
+
+			Console.WriteLine();
+
+			Console.WriteLine(new string('=', 50));
+		}
+
+		static PowerSet<int> CreatePowerSet(int startValue, int finishValue)
+		{
+			PowerSet<int> resultSet = new PowerSet<int>();
+			for (int i = startValue; i <= finishValue; i++)
+				resultSet.Put(i);
+
+			return resultSet;
+		}
+
+		static PowerSet<int> CreateEmptyPowerSet()
+		{
+			return new PowerSet<int>();
 		}
 	}
 }
